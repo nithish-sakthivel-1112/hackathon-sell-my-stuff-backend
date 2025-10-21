7bbbed5bcb8b34be5d028a97386c2a504d866853
NithishSakthivel <nithish0829@gmail.com>
2025-10-22 00:30:52 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-tfstate-backend-sellmystuff"
+    bucket = "terraform-tfstate-backend-1"
     key    = "S3/terraform.tfstate"
     region = "eu-central-1"
   }
